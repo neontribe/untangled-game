6a361cbc4fba20a38aae9acc0c5a73651e6a6544
Hoolean <me@hoolean.com>
2017-08-07 21:34:49 +0100

    Fix pip python package permissions once and for all

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ fi
 # python and pip3
 sudo apt-get -y install python3 python3-pip
 # python dependency packages
-pip3 install -r requirements.txt
+pip3 install --user -r requirements.txt
 # pyre dependency package from github
-pip3 install https://github.com/zeromq/pyre/archive/master.zip
+pip3 install --user https://github.com/zeromq/pyre/archive/master.zip

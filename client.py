3a4dd03f6c28971c83d3cc2ed33f546fe92ebd7e
Matthew Williams <mw999@hotmail.co.uk>
2017-08-11 12:53:04 +0100

    Replaced map with original one.

--- client.py
+++ client.py
@@ -105,7 +105,7 @@ class GameClient():
             pygame.locals.KEYDOWN, pygame.locals.MOUSEBUTTONDOWN,  pygame.locals.JOYBUTTONDOWN])
 
         self.levels = {
-            "main": SaveLevel('./assets/maps/patchy')
+            "main": SaveLevel('./assets/maps/CAPFLAG MAP NAT')
         }
 
         self.map = Map(
@@ -406,11 +406,11 @@ class GameClient():
                         self.network.node.shout("world:combat", bson.dumps(me.cast_spells[-1].get_properties()._asdict()))
                         self.cast = False
 
-                    
+
                     for playerUUID, player in self.players.others.items():
                         try:
                             player.render()
-                            
+
                             for spell in player.cast_spells:
                                 spell.render()
                                 hit_me = spell.hit_target_player(me)
@@ -421,7 +421,7 @@ class GameClient():
                         except PlayerException as e:
                             # PlayerException due to no initial position being set for that player
                             pass
-                    
+
                     score_shift = 220
                     for team, score in self.scores.items():
                         colour = (0, 0, 200) if team == 'blue' else (200, 0, 0)

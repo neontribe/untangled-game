ee6d5e71681eb9e81e9fe2979276b255408ac697
Oliver Barnwell <barnwelloliver@gmail.com>
2018-08-05 13:29:05 +0100

    Improve windows install and launch experience

--- untangled-2018/install.bat
+++ untangled-2018/install.bat
@@ -1,3 +1,9 @@
 @echo off
 
 py -m pip install --user -r requirements.txt
+
+echo What would you like the hostname to be?
+set /p HOSTNAME=
+@echo %HOSTNAME%
+
+@echo HOSTNAME="%HOSTNAME%"> ecs/config.py
---
+++ untangled-2018/launch.bat
@@ -0,0 +1,3 @@
+@echo off
+
+py untangled.py

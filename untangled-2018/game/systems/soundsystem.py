76e92e213a255ae5a72258750db604a03cc122d1
Spannner <hanthony28n@gmail.com>
2018-08-06 14:15:52 +0100

    Missing files

---
+++ untangled-2018/game/systems/soundsystem.py
@@ -0,0 +1,19 @@
+from lib.system import System
+from game.components import *
+import pygame
+
+class SoundSystem(System):
+    def __init__(self):
+        self.playingMusic = ""
+        self.STOPPLAYINGEVENT = pygame.USEREVENT+1
+    def update(self, game, dt, events):
+        for key,entity in game.entities.items():
+            if BackgroundMusic in entity:
+                path = entity[BackgroundMusic].path
+                if self.playingMusic != path:
+                    pygame.mixer.music.stop()
+                    pygame.mixer.music.load(path)
+                    pygame.mixer.music.play()
+                    self.playingMusic = path
+                elif not pygame.mixer.music.get_busy():
+                    pygame.mixer.music.play()

a60e4fd098b38d25c5291dc1f4ee69a44b8fbd32
George Sima <bogdansima20@gmail.com>
2018-08-08 13:41:04 +0100

    Fixed the bug that crashes the game

--- untangled-2018/game/systems/inventorysystem.py
+++ untangled-2018/game/systems/inventorysystem.py
@@ -10,18 +10,19 @@ class InventorySystem(System):
     def itemPickedUp(self, game, event, key):
         itemEntity, itemKey = self.getItemFromEvent(game, event, key)
 
-        if not itemEntity[CanPickUp].pickedUp:
-            playerEntity = game.entities[key]
-            
-            if itemKey not in playerEntity[Inventory].items:
-                playerEntity[Inventory].items.append(itemKey)
-                playerEntity[Inventory].items.append(itemEntity[CanPickUp].quantity)
-            else:
-                itemIndexInList = playerEntity[Inventory].items.index(itemKey)
-                playerEntity[Inventory].items[itemIndexInList + 1] += 1
+        if CanPickUp in itemEntity:
+            if not itemEntity[CanPickUp].pickedUp:
+                playerEntity = game.entities[key]
+                
+                if itemKey not in playerEntity[Inventory].items:
+                    playerEntity[Inventory].items.append(itemKey)
+                    playerEntity[Inventory].items.append(itemEntity[CanPickUp].quantity)
+                else:
+                    itemIndexInList = playerEntity[Inventory].items.index(itemKey)
+                    playerEntity[Inventory].items[itemIndexInList + 1] += 1
 
-            itemEntity[CanPickUp].pickedUp = True
-            playerEntity[Inventory].activeItem = (itemKey, itemEntity[CanPickUp].quantity)
+                itemEntity[CanPickUp].pickedUp = True
+                playerEntity[Inventory].activeItem = (itemKey, itemEntity[CanPickUp].quantity)
 
     def getItemFromEvent(self, game, event, playerkey):
         for k in event.keys:
